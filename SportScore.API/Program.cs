171aaad250f5648f9995e2b26b7d8126c35eaae0
Kristian Dimitrov <krisdimitrov03@students.softuni.bg>
2022-06-20 05:17:12 +0300

    Infrastructure Project and Identity Added

--- SportScore.API/Program.cs
+++ SportScore.API/Program.cs
@@ -2,8 +2,19 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 using SportScore.API.SportsDataOperators.Contracts;
 using SportScore.API.SportsDataOperators.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SportScore.Infrastructure.Data;
+using SportScore.Infrastructure.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("SportScoreContextConnection") ?? throw new InvalidOperationException("Connection string 'SportScoreContextConnection' not found.");
+
+builder.Services.AddDbContext<SportScoreContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<SportScoreContext>();;
 
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
---
+++ SportScore.Infrastructure/Data/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportScore.Infrastructure.Data.Models;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
